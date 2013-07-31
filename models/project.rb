82487c0325332a2054d2b2ab730cc9c758559841
Dan Mayer <dan@mayerdan.com>
2013-07-30 23:18:56 -0400

    update

--- models/project.rb
+++ models/project.rb
@@ -7,7 +7,7 @@ class Project
 
   def self.add_project(name, data)
     REDIS.hset(REDIS_KEY, name, data.to_json)
-    Project.new(name, data)
+    Project.new(name, data.to_json)
   end
 
   def self.remove_project(name)
