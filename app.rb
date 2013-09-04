740d56c912bf3363fa1514e58336685af3725101
Dan Mayer <dan@mayerdan.com>
2013-09-03 23:27:24 -0400

    chart test
    added files
    
    removed file

--- app.rb
+++ app.rb
@@ -118,6 +118,27 @@ post '/churn/*' do |project_path|
   end
 end
 
+get '/chart/*' do |project_path|
+  @project      = Project.get_project(project_path)
+
+  # @project.sorted_commits.map{ |commit|
+  #   churn_results = commit.churn_results 
+  #   churn_results.exists? ? churn_results.file_changes : nil
+  # }.compact.inspect
+
+  @chartdata = {
+    labels: ["January","February","March","April","May","June","July"],
+    datasets: [
+      {
+	fillColor: "rgba(151,187,205,0.5)",
+	strokeColor: "rgba(151,187,205,1)",
+	data: [28,48,40,19,96,27,100]
+      }
+    ]
+  }
+  erb :chart
+end 
+
 get '/*' do |project_path|
   @project      = Project.get_project(project_path)
   if @project
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,3 +1,14 @@
 (function ($) {
   console.log('ready')
+
+  $(".chart-loading").load($(".chart-loading").data('url'));
+
+  if($("#BarChart").length > 0) {
+    var ctx = $("#BarChart").get(0).getContext("2d");
+    var data = $("#BarChart").data('bardata')
+    new Chart(ctx).Bar(data);
+  }
+
+
+
 }(jQuery));
