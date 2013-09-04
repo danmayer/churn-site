f647619837363bc4f855f827acc239c900789972
Dan Mayer <dan@mayerdan.com>
2013-09-03 23:36:53 -0400

    JS on draw

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,14 +1,12 @@
 (function ($) {
   console.log('ready')
 
-  $(".chart-loading").load($(".chart-loading").data('url'));
-
-  if($("#BarChart").length > 0) {
-    var ctx = $("#BarChart").get(0).getContext("2d");
-    var data = $("#BarChart").data('bardata')
-    new Chart(ctx).Bar(data);
-  }
-
-
+  $(".chart-loading").load($(".chart-loading").data('url'), function(){
+    if($("#BarChart").length > 0) {
+      var ctx = $("#BarChart").get(0).getContext("2d");
+      var data = $("#BarChart").data('bardata')
+      new Chart(ctx).Bar(data);
+    }
+  });
 
 }(jQuery));
