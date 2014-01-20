e7f9dfb09920eb8442d9e928efd1454b6af0b4ef
Dan Mayer <dan.mayer@livingsocial.com>
2014-01-20 13:13:07 -0500

    indenting

--- app.rb
+++ app.rb
@@ -92,13 +92,12 @@ get '/docs/?' do
   redirect '/docs/index.html'
 end
 
-
 # returns the api docs for the resource listing
-  get '/api-docs/?', :provides => [:json] do
-    res = File.read(File.join('public', 'api', 'api-docs.json'))
-    body res
-    status 200
-  end
+get '/api-docs/?', :provides => [:json] do
+  res = File.read(File.join('public', 'api', 'api-docs.json'))
+  body res
+  status 200
+end
 
 # returns the api docs for each path
 get '/api-docs/:api', :provides => [:json] do
