4a5f213d41bbae0d7c6eca948dc28b80812d1fc7
Dan Mayer <dan.mayer@livingsocial.com>
2014-01-26 14:03:52 -0500

    try json body post for the api

--- app.rb
+++ app.rb
@@ -309,15 +309,15 @@ end
 ##~ op.type = "string"
 ##~ op.set :method => "POST", :deprecated => false, :nickname => "create_project"
 ##~ op.summary = "creates a new churn project by project_name"
-##~ op.parameters.add :name => "project_name", :description => "The project_name of the churn project to be created", :type => "string", :allowMultiple => false, :required => true, :paramType => "query"
+##~ op.parameters.add :name => "project_name", :description => "The project_name of the churn project to be created", :type => "string", :allowMultiple => false, :required => true, :paramType => "body"
 ##
 post '/projects/add', :provides => [:html, :json] do
   # #allow for json body posts
-  # request.body.rewind
-  # request_payload = JSON.parse request.body.read rescue nil
-  # if request_payload
-  #  params = request_payload
-  # end
+  request.body.rewind
+  request_payload = JSON.parse request.body.read rescue nil
+  if request_payload
+   params = request_payload
+  end
 
   project_name = params['project_name']
   #fix starting with a slash if cleint passed with a slash
